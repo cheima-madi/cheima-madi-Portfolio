4ab4d33dafe966b57ae3e2cc42e4659830d408f2
cheima <cheima.madi@univ-constantine2.dz>
2026-02-22 00:21:04 +0100

    Fix: Add missing @assets path to tsconfig and sync repo case sensitivity

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/cheima-madi-portfolio/",
+  base: "/cheima-madi-Portfolio/",
   plugins: [react()],
   resolve: {
     alias: {
